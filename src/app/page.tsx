3566ad081081fb53bede596fe9d8550928d5583c
Mallika Chumchinda <>
2025-05-09 23:13:35 +0700

    fix: fetch data with service api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,24 @@
+'use client'
+
 import Head from 'next/head'
 import Navbar from '@/components/Navbar'
 import HeroBanner from '@/components/HeroBanner'
 import PopularSwiper from '@/features/movies/components/PopularSwiper'
+import { useState } from 'react'
+import { useMovieSearch } from '@/features/movies/hooks/useMovieSearch'
 
 export default function Home() {
-  const mockMovies = [
-    {
-      id: 1,
-      title: 'Stranger Things',
-      poster_path: '1.png',
-    },
-    {
-      id: 2,
-      title: 'The Witcher',
-      poster_path: '2.png',
-    },
-    {
-      id: 3,
-      title: 'You',
-      poster_path: '3.png',
-    },
-    {
-      id: 4,
-      title: 'The Crown',
-      poster_path: '4.png',
-    },
-    {
-      id: 5,
-      title: 'Money Heist',
-      poster_path: '5.png',
-    },
-    {
-      id: 6,
-      title: 'Lucifer',
-      poster_path: '6.png',
-    },
-    {
-      id: 7,
-      title: 'Breaking Bad',
-      poster_path: '7.png',
-    },
-  ]
+  const [query, setQuery] = useState('movie')
+  const { movies, loading, error } = useMovieSearch(query)
 
   return (
     <main className="bg-black min-h-screen">
       <Head>
-        <title>Netflix Clone</title>
+        <title>Nextflix - Web Application</title>
       </Head>
       <Navbar />
       <HeroBanner />
-      <PopularSwiper title="Popular on Netflix" movies={mockMovies} />
+      <PopularSwiper title="Popular on Netflix" movies={movies} />
     </main>
   )
 }
