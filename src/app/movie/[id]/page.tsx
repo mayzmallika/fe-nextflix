60281c85203ba8e10c29c149f8ba389f66be77b9
Mallika Chumchinda <>
2025-05-10 15:04:50 +0700

    fix: add .env api url

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -3,9 +3,15 @@ import Navbar from '@/components/Navbar'
 import { MovieDetailType } from '@/features/movies/types'
 import { notFound } from 'next/navigation'
 
+type MovieDetailProps = {
+  params: {
+    id: string
+  }
+}
+
 async function getMovie(id: string): Promise<MovieDetailType | null> {
   try {
-    const res = await fetch(`http://localhost:3001/movies/detail?t=${id}`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/movies/${id}`, {
       cache: 'no-store',
     })
 
@@ -17,11 +23,8 @@ async function getMovie(id: string): Promise<MovieDetailType | null> {
     return null
   }
 }
-export default async function MovieDetail({
-  params,
-}: {
-  params: { id: string }
-}) {
+
+export default async function MovieDetail({ params }: MovieDetailProps)  {
   const movie = await getMovie(params.id)
   if (!movie) return notFound()
 
@@ -45,7 +48,10 @@ export default async function MovieDetail({
           </p>
           {/* <p className="text-lg mb-4 max-w-xl line-clamp-4 md:line-clamp-5 lg:line-clamp-6"> */}
 
-          <p className="text-lg mb-4 mx-2 line-clamp-4 md:line-clamp-5 lg:line-clamp-6"> {movie.plot}</p>
+          <p className="text-lg mb-4 mx-2 line-clamp-4 md:line-clamp-5 lg:line-clamp-6">
+            {' '}
+            {movie.plot}
+          </p>
           <button className="px-6 py-2 bg-red-600 hover:bg-red-700 text-white font-semibold rounded-xl">
             ▶ Play
           </button>
--- src/features/movies/services/movieService.ts
+++ src/features/movies/services/movieService.ts
@@ -1,7 +1,7 @@
 import { Movie } from '../types'
 
 export async function fetchMovies(query: string): Promise<Movie[]> {
-  const res = await fetch(`http://localhost:3001/movies/search?q=${query}`)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/movies?keyword=${query}`)
   if (!res.ok) throw new Error('Failed to fetch movies')
   const data = await res.json()
   return data.data
